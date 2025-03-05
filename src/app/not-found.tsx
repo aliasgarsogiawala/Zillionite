14f3d1f2aa3993c8d68fcb37de14e9264e916c6d
aliasgarsogiawala <vertexxinfotech18@gmail.com>
2025-03-05 12:56:00 +0530

    update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -20,7 +20,7 @@ export default function NotFound() {
         </h1>
         
         <p className="text-gray-600 mb-8">
-          The page you're looking for doesn't exist or has been moved.
+          The page your&apos;e looking for doesn&apos;t exist or has been moved.
         </p>
         
         <Link 
