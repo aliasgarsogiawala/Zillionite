75a8d0ca258ed34ef094d6874e69a1997d697443
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2025-03-10 20:43:33 +0530

    horizontal overflow fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className="overflow-x-hidden">
         <Layout>{children}</Layout>
